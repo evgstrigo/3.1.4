1d1819e14d76002748e4a8c9d99f45b068207133
Evgeniy Strigo <evgstrigo@mail.ru>
2022-03-27 15:32:46 +0300

    добавил 2 аннотации в репо

--- src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
@@ -1,11 +1,15 @@
 package ru.kata.spring.boot_security.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
+@Component
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Role findByValue(String value);
 }
